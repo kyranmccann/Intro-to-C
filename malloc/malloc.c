48d492c364ee812705d4b9982521293ed51a1e00
kyranmccann <kyran.mccann@gmail.com>
2019-03-18 22:34:14 -0700

    complete malloc

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -4,7 +4,7 @@
 #include "lib.h"
 
 /*
-    Duplicates the input string by dynamically allocating memory for 
+    Duplicates the input string by dynamically allocating memory for
     the duplicate string using `malloc` and then copying the string
     into the allocated memory. Returns a pointer to the allocated memory.
     You may want to use the string_length function to figure out the
@@ -12,7 +12,12 @@
 */
 char *string_dup(char *src)
 {
-
+  int length = string_length(src);
+  char *copy = malloc(length);
+  for (int i = 0; i <= length; i++){
+    copy[i] = src[i];
+  }
+  return copy;
 }
 
 /*
@@ -20,27 +25,37 @@ char *string_dup(char *src)
     of any type and copies its contents to the destination pointer (dest).
     You may want to cast the input pointers to char pointers first before
     performing the copying. `n` is the amount of data that should be copied
-    from `src` to `dest`. 
+    from `src` to `dest`.
 */
 void mem_copy(void *dest, const void *src, int n)
 {
-
+  char *start = (char *)src;
+  char *end = dest;
+  for (int i = 0; i < n; i++) {
+    end[i] = start[i];
+  };
 }
 
 /*
-    Given a pointer of `malloc`'d memory, this function will 
+    Given a pointer of `malloc`'d memory, this function will
     attempt to resize the allocated memory to the new specified
-    size. Any data that was previously in the old `malloc`'d 
-    memory should be intact in the new resized block of memory. 
+    size. Any data that was previously in the old `malloc`'d
+    memory should be intact in the new resized block of memory.
     Some edge cases to consider: how should resizing be handled
-    in the case when old_size < new_size? What about when 
+    in the case when old_size < new_size? What about when
     old_size > new_size?
-    
+
     Do not use the `realloc` function from the standard libary.
 */
 void *resize_memory(void *ptr, int old_size, int new_size)
 {
-
+  void *dest = malloc(new_size);
+    for (int i = 0; i < old_size && i < new_size; i++)
+    {
+        // *((char *)(dest + i)) = *((char *)(ptr + i));
+        ((char *)dest)[i] = ((char *)ptr)[i];
+    }
+    return dest;
 }
 
 #ifndef TESTING
@@ -54,7 +69,7 @@ int main(void)
     int numbers[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
     int n = sizeof(numbers) / sizeof(numbers[0]);
     int *target = malloc(n * sizeof(int));
-    
+
     mem_copy(target, numbers, n * sizeof(int));
 
     printf("Copied array: ");
@@ -69,7 +84,7 @@ int main(void)
     char *path = string_dup("/students/");
     int url_length = string_length(url);
     int path_length = string_length(path);
-    
+
     int new_length = url_length - 1 + path_length;
     char *new_url = resize_memory(url, url_length, new_length);
     char *p = new_url + url_length;
